66e0a222288f32120ef2a9f1bba965615c1c989b
Roman Avdeienko <avroman91@gmail.com>
2021-04-23 20:00:40 +0300

    HW 8 task for 100 added (with a ArrayList and if-else cycles

---
+++ src/com/lesson8/Homework8TaskFor100.java
@@ -0,0 +1,50 @@
+package com.lesson8;
+
+import java.util.ArrayList;
+
+/*
+Баланс скобок. Дана конечная последовательность, состоящая из левых и правых скобок различных заданных типов.
+Определить, можно ли добавить в нее цифры и знаки арифметических действий так, чтобы получилось правильное арифметическое выражение.
+*/
+
+public class Homework8TaskFor100 {
+    public static void main(String[] args) {
+        String symbols = "{([])}";
+        String check = "()[]{}";
+        System.out.println(checkForCorrectSymblols(symbols));
+        System.out.println(checkForCorrectSymblols(check));
+
+    }
+
+    public static boolean checkForCorrectSymblols(String string) {
+        ArrayList<Character> array = new ArrayList<>();
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '{' || string.charAt(i) == '[' || string.charAt(i) == '(') {
+                array.add(string.charAt(i));
+                continue;
+            }
+            if (string.charAt(i) == '}' || string.charAt(i) == ']' || string.charAt(i) == ')') {
+                if (string.charAt(i) == '}') {
+                    if (array.get(array.size() - 1) == '{') {
+                        array.remove(array.size() - 1);
+                    } else {
+                        return false;
+                    }
+                } else if (string.charAt(i) == ']') {
+                    if (array.get(array.size() - 1) == '[') {
+                        array.remove(array.size() - 1);
+                    } else {
+                        return false;
+                    }
+                } else if (string.charAt(i) == ')') {
+                    if (array.get(array.size() - 1) == '(') {
+                        array.remove(array.size() - 1);
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+        return array.isEmpty();
+    }
+}
