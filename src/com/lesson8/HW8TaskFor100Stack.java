0d5d5b93d9c6ab53f72503306ea1639748fb40fc
Roman Avdeienko <avroman91@gmail.com>
2021-04-23 20:37:08 +0300

    HW8 task for 100 stack - rename.

---
+++ src/com/lesson8/HW8TaskFor100Stack.java
@@ -0,0 +1,47 @@
+package com.lesson8;
+
+import java.util.Stack;
+
+public class HW8TaskFor100Stack {
+    public static void main(String[] args) {
+        String symbols = "{([])}";
+        String check = "())[]{}";
+        System.out.println(checkForCorrectSymblols(symbols));
+        System.out.println(checkForCorrectSymblols(check));
+
+    }
+
+    public static boolean checkForCorrectSymblols(String string) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) == '{' || string.charAt(i) == '[' || string.charAt(i) == '(') {
+                stack.push(string.charAt(i));
+                continue;
+            }
+            if (stack.isEmpty()) {
+                return false;
+            }
+            if (string.charAt(i) == '}') {
+                if (stack.peek() == '{') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (string.charAt(i) == ']') {
+                if (stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            } else if (string.charAt(i) == ')') {
+                if (stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
