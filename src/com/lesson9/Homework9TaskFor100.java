c9485d9a2aef54a9e644e9095f7cf773210ebb88
Roman Avdeienko <avroman91@gmail.com>
2021-04-27 12:58:16 +0300

    HW9 task for 100 - with center.

--- src/com/lesson9/Homework9TaskFor100.java
+++ src/com/lesson9/Homework9TaskFor100.java
@@ -14,8 +14,8 @@ public class Homework9TaskFor100 {
 
     public static String returnListOfPointsInsideOfACircle(int[] center, int radius) {
         ArrayList<String> list = new ArrayList<String>();
-        for (int i = -radius + 1; i < radius; i++) {
-            for (int j = -radius + 1; j < radius; j++) {
+        for (int i = center[0] - radius + 1; i < center[0] + radius; i++) {
+            for (int j = center[1] - radius + 1; j < center[0] + radius; j++) {
                 if (isInCircle(center, i, j, radius)) {
                     list.add("(" + i + "," + j + ")");
                 }
