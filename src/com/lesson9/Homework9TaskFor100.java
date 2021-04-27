55d4ce59665d98628dfc9818ff83d3207b17a125
Roman Avdeienko <avroman91@gmail.com>
2021-04-27 12:46:57 +0300

    HW9 task for 100 - initial commit.

---
+++ src/com/lesson9/Homework9TaskFor100.java
@@ -0,0 +1,30 @@
+package com.lesson9;
+
+/*Задан круг с радиусом r с центром в точке (x,y).
+Вернуть список точек с целочисленными координатами которые попадают в круг.*/
+
+import java.util.ArrayList;
+
+public class Homework9TaskFor100 {
+    public static void main(String[] args) {
+        int[] center = {0, 0};
+        int raduis = 5;
+        System.out.println(returnListOfPointsInsideOfACircle(center, raduis));
+    }
+
+    public static String returnListOfPointsInsideOfACircle(int[] center, int radius) {
+        ArrayList<String> list = new ArrayList<String>();
+        for (int i = -radius + 1; i < radius; i++) {
+            for (int j = -radius + 1; j < radius; j++) {
+                if (isInCircle(center, i, j, radius)) {
+                    list.add("(" + i + "," + j + ")");
+                }
+            }
+        }
+        return list.toString();
+    }
+
+    public static boolean isInCircle(int[] center, int pointX, int pointY, int radius) {
+        return (pointX - center[0]) * (pointX - center[0]) + (pointY - center[1]) * (pointY - center[1]) < radius * radius;
+    }
+}
