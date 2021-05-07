938e2cbbf9244a313f9253df7cb3c8b89f2a9256
Roman Avdeienko <avroman91@gmail.com>
2021-05-07 22:52:42 +0300

    HW11 Task 1 initial commit.

--- src/com/lesson11/CreateRandomPerson.java
+++ src/com/lesson11/CreateRandomPerson.java
@@ -4,17 +4,17 @@ import java.util.Random;
 
 public class CreateRandomPerson {
     Random random = new Random();
-    private int age = random.nextInt(10)+10;
-    private int height = random.nextInt(70)+100;
-    private int weight = random.nextInt(40)+40;
+    private int age = random.nextInt(10) + 10;
+    private int height = random.nextInt(70) + 100;
+    private int weight = random.nextInt(40) + 40;
     private String[] names = {"Will", "Mike", "Dustin", "Eleven", "Lucas", "Max"};
     private String[] familyNames = {"Byers", "Wheeler", "Handerson", " ", "Sinclair", "Mayfield"};
     private int tmp = random.nextInt(6);
     private String name = names[tmp];
     private String familyName = familyNames[tmp];
 
-    public Person randomPerson (){
-        Person person = new Person(name,familyName,age, weight, height );
+    public Person randomPerson() {
+        Person person = new Person(name, familyName, age, weight, height);
         return person;
     }
 
