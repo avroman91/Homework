1d42f90e8aa9c167d8f41510bcb7d8b3135a0739
Roman Avdeienko <avroman91@gmail.com>
2021-05-07 23:27:34 +0300

    HW11 Task 2 initial commit.

---
+++ src/com/lesson11/HW11Task2.java
@@ -0,0 +1,24 @@
+package com.lesson11;
+
+public class HW11Task2 {
+
+    private static int[][] matrix = {{14, 15, 16, 17}, {10, 11, 12, 13}, {22, 23, 24, 25}, {18, 19, 20, 21}};
+
+    public static void main(String[] args) {
+        MatrixOperations matrixOperations = new MatrixOperations();
+
+//        matrixOperations.swapRows(matrix, 0,1);
+//        matrixOperations.swapRows(matrix, 2,3);
+//
+//        matrixOperations.swapColumn(matrix,0,1);
+//
+//        matrixOperations.mul(matrix,2);
+//
+//        matrixOperations.print(matrix);
+//
+//        int [] array = matrixOperations.array(matrix);
+//        System.out.println(Arrays.toString(array));
+//
+//        System.out.println(matrixOperations.max(matrix));
+    }
+}
---
+++ src/com/lesson11/MatrixOperations.java
@@ -0,0 +1,69 @@
+package com.lesson11;
+
+/*
+print(int[] arr) - печатает матрицу
+swapColumns(int arr[], i, j) - меняет 2 столбца местами
+swapRows(int arr[], i, j) - меняет 2 рядка местами
+mul(int[] arr, int value) - умножает матрицу на число
+toArray(int[] arr) - превращает матрицу в массив. Например если была матрица на 3x4 то вернуть массив из 12 элемнтов
+max(int[] arr) - возвращает максимальный элемент матрицы
+*/
+
+import java.util.Arrays;
+
+public class MatrixOperations {
+
+
+    public void print(int[][] matrix) {
+        for (int[] ints : matrix) {
+            System.out.println(Arrays.toString(ints));
+        }
+        ;
+    }
+
+    public void swapRows(int[][] matrix, int i, int j) {
+        int[] tmp = matrix[i];
+        matrix[i] = matrix[j];
+        matrix[j] = tmp;
+    }
+
+    public void swapColumn(int[][] matrix, int i, int j) {
+        for (int k = 0; k < matrix[0].length; k++) {
+            int tmp = matrix[k][i];
+            matrix[k][i] = matrix[k][j];
+            matrix[k][j] = tmp;
+        }
+    }
+
+    public void mul(int[][] matrix, int mul) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                matrix[i][j] = matrix[i][j] * mul;
+            }
+        }
+    }
+
+    public int[] array(int[][] matrix) {
+        int[] array = new int[matrix.length * matrix[0].length];
+        int index = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                array[index] = matrix[i][j];
+                index++;
+            }
+        }
+        return array;
+    }
+
+    public int max(int[][] matrix) {
+        int maxValue = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (maxValue < matrix[i][j]) {
+                    maxValue = matrix[i][j];
+                }
+            }
+        }
+        return maxValue;
+    }
+}
