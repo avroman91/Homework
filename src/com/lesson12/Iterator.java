50e38758e0f2bb895d1bb2d23791c02072e2f6c2
Roman Avdeienko <avroman91@gmail.com>
2021-05-18 22:36:15 +0300

    TaskFor100 added (Iterator & TestIterator).

---
+++ src/com/lesson12/Iterator.java
@@ -0,0 +1,58 @@
+package com.lesson12;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Iterator {
+
+    private List<Integer> listForOut = new ArrayList<>();
+    private int internalCounter = 0;
+
+    public Iterator(List<List<Integer>> lists) {
+
+        for (int i = 0; i < lists.size(); i++) {
+            int counterListOne = 0;
+            int counterListTwo = 0;
+            while (i == 0) {
+                if (counterListOne < lists.get(0).size() && lists.get(0).get(counterListOne) <= lists.get(1).get(counterListTwo)) {
+                    listForOut.add(lists.get(0).get(counterListOne));
+                    counterListOne++;
+                } else if (counterListTwo < lists.get(1).size() && lists.get(1).get(counterListOne) >= lists.get(1).get(counterListTwo)) {
+                    listForOut.add(lists.get(1).get(counterListTwo));
+                    counterListTwo++;
+                } else {
+                    i += 2;
+                    counterListOne = 0;
+                    counterListTwo = 0;
+                }
+            }
+            while (true) {
+                if (counterListTwo < lists.get(i).size() && counterListOne < listForOut.size() && listForOut.get(counterListOne) < lists.get(i).get(counterListTwo)) {
+                    counterListOne++;
+                } else if (counterListOne == listForOut.size() || counterListTwo < lists.get(i).size() && counterListOne < listForOut.size() && counterListTwo < lists.get(i).get(counterListTwo) && listForOut.get(counterListOne) >= lists.get(i).get(counterListTwo)) {
+                    listForOut.add(counterListOne, lists.get(i).get(counterListTwo));
+                    counterListTwo++;
+                } else {
+                    counterListOne = 0;
+                    counterListTwo = 0;
+                    break;
+                }
+            }
+
+        }
+
+
+    }
+
+
+    public boolean hasNext() {
+        return internalCounter < listForOut.size();
+    }
+
+    public Integer next() {
+        int integerFromList = listForOut.get(internalCounter);
+        internalCounter++;
+        return integerFromList;
+    }
+
+}
---
+++ src/com/lesson12/TestIterator.java
@@ -0,0 +1,22 @@
+package com.lesson12;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class TestIterator {
+
+    public static void main(String[] args) {
+        List<List<Integer>> lists = new ArrayList<>();
+        lists.add(Arrays.asList(1, 3, 5));
+        lists.add(Arrays.asList(2, 2, 3, 6));
+        lists.add(Arrays.asList(1, 3, 7));
+
+        Iterator iterator = new Iterator(lists);
+
+        while (iterator.hasNext()) {
+            System.out.print(iterator.next() + " ");
+        }
+        // 1, 1, 2, 2, 3, 3, 5, 6, 7
+    }
+}
