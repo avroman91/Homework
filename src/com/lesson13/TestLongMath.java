9338c15d5c30433dd51f4c1c4f501c22397e025e
Roman Avdeienko <avroman91@gmail.com>
2021-05-19 23:48:07 +0300

    LongMath.dev initial commit.

--- src/com/lesson13/TestLongMath.java
+++ src/com/lesson13/TestLongMath.java
@@ -2,8 +2,11 @@ package com.lesson13;
 
 public class TestLongMath {
     public static void main(String[] args) {
-        String s = LongMath.sum("124","456");
-        String d = LongMath.dev("124","456"); //-332
-        System.out.println(d);
+        String s = LongMath.sum("124","456"); // 580
+        String d1 = LongMath.dev("124","456"); //-332
+        String d2 = LongMath.dev("524","456"); // 068
+        System.out.println(s);
+        System.out.println(d1);
+        System.out.println(d2);
     }
 }
