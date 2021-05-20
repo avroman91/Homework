26b2d75beddc9f67a57429f86a518269182c5938
Roman Avdeienko <avroman91@gmail.com>
2021-05-20 19:03:02 +0300

    sum & sub seems working good.

--- src/com/lesson13/LongMath.java
+++ src/com/lesson13/LongMath.java
@@ -21,27 +21,33 @@ public class LongMath {
         return st.toString();
     }
 
-    public static String dev(String x, String y) {
+    public static String substraction(String x, String y) {
         StringBuilder tmp = new StringBuilder();
         if (x.length() == y.length()) {
             for (int i = 0; i < x.length() - 1; i++) {
                 if (Integer.parseInt(String.valueOf(x.charAt(i))) > Integer.parseInt(String.valueOf(y.charAt(i)))) {
-                    tmp = devGreaterMinusSmaller(x,y);
+                    tmp = greaterMinusSmallerValue(x, y);
                     break;
                 } else if (Integer.parseInt(String.valueOf(x.charAt(i))) < Integer.parseInt(String.valueOf(y.charAt(i)))) {
-                    tmp = devGreaterMinusSmaller(y,x);
+                    tmp = greaterMinusSmallerValue(y, x);
                     tmp.insert(0, "-");
                     break;
                 }
             }
+        } else if (x.length() > y.length()) {
+            tmp = greaterMinusSmallerValue(x, y);
+        } else {
+            tmp = greaterMinusSmallerValue(y, x);
+            tmp.insert(0, "-");
         }
         return tmp.toString();
     }
 
-    private static StringBuilder devGreaterMinusSmaller(String x, String y) {
+
+    private static StringBuilder greaterMinusSmallerValue(String x, String y) {
         StringBuilder st = new StringBuilder();
         int doz = 0;
-        for (int i = 1; i < x.length() + 1; i++) {
+        for (int i = 1; i < y.length() + 1; i++) {
             int tmp = Integer.parseInt(String.valueOf(x.charAt(x.length() - i))) - Integer.parseInt(String.valueOf(y.charAt(y.length() - i))) - doz;
             if (tmp < 0) {
                 doz = 1;
@@ -50,6 +56,19 @@ public class LongMath {
                 doz = 0;
             }
             st.insert(0, tmp);
+        }
+        if (st.length() < x.length() && doz == 0) {
+            int difference = x.length() - st.length();
+            for (int i = 0; i < difference; i++) {
+                st.insert(i, x.charAt(i));
+            }
+        }
+        while (true) {
+            if (Integer.parseInt(String.valueOf(st.charAt(0))) == 0) {
+                st.delete(0, 1);
+            } else {
+                break;
+            }
         }
         return st;
     }
--- src/com/lesson13/TestLongMath.java
+++ src/com/lesson13/TestLongMath.java
@@ -3,10 +3,12 @@ package com.lesson13;
 public class TestLongMath {
     public static void main(String[] args) {
         String s = LongMath.sum("124","456"); // 580
-        String d1 = LongMath.dev("124","456"); //-332
-        String d2 = LongMath.dev("524","456"); // 068
+        String d1 = LongMath.substraction("124","456"); //-332
+        String d2 = LongMath.substraction("524","456"); // 068
+        String d3 = LongMath.substraction("1524","456"); // 1068
         System.out.println(s);
         System.out.println(d1);
         System.out.println(d2);
+        System.out.println(d3);
     }
 }
