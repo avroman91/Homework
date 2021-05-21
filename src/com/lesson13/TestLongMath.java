b317bcfbbd81d5f8672de6f8b481d4e444045326
Roman Avdeienko <avroman91@gmail.com>
2021-05-22 01:03:16 +0300

    mult initial commit. Я хз как, но вроде работает.

--- src/com/lesson13/TestLongMath.java
+++ src/com/lesson13/TestLongMath.java
@@ -5,7 +5,7 @@ public class TestLongMath {
         String sum = LongMath.sum("195", "15"); // 580
         String sub = LongMath.subtraction("124", "456"); //-332
         String div = LongMath.division("1524", "456"); //3
-        String mult = LongMath.mult("135454", "5"); //3
+        String mult = LongMath.mult("135454", "5"); //677270
         System.out.println(sum);
         System.out.println(sub);
         System.out.println(div);
