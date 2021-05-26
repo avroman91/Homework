c3013a2c597a4c2b932db1d8165ae031a937a4f4
Roman Avdeienko <avroman91@gmail.com>
2021-05-27 00:09:19 +0300

    reformated + added initial task

--- src/com/lesson14/task2/Main.java
+++ src/com/lesson14/task2/Main.java
@@ -2,20 +2,40 @@ package com.lesson14.task2;
 
 import org.apache.commons.io.FileUtils;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.*;
 
+/*
+2*) Есть документ со списком URL:
+https://drive.google.com/open?id=1wVBKKxpTKvWwuCzqY1cVXCQZYCsdCXTl
+Вывести топ 10 доменов которые встречаются чаще всего.
+В документе могут встречатся пустые и недопустимые строки.
+Например для URL m.youtube.com/watch?v=SHH_EMeRjGA
+доменом будет m.youtube.com
+
+Задание на 100 баллов:
+сделать дз 14.2 (вывести 10 доменов которые встречаются чаще всего), но при этом файл не хранить в файловой системе а зпгружать по прямой ссылке ссылке программно:
+https://dl.dropboxusercontent.com/s/21rtgfdmlp6wycx/urls.txt
+*/
 public class Main {
 
-    public static void main(String[] args)  {
-        String result = readFromFile("urls.txt");
-        String[] urls = result.split("\\r?\\n");
+    public static void main(String[] args) throws Exception {
+
+        URL url = new URL("https://dl.dropboxusercontent.com/s/21rtgfdmlp6wycx/urls.txt");
+        BufferedReader read = new BufferedReader(
+                new InputStreamReader(url.openStream()));
+        String tmp;
         List<String> urlList = new ArrayList<>();
-        for (String tmpUrl : urls) {
-            tmpUrl = tmpUrl.replaceAll("\\s+", "");
-            urlList.add(UrlSeparator.getUrl(tmpUrl).toLowerCase(Locale.ROOT));
+        while ((tmp = read.readLine()) != null) {
+            tmp = tmp.replaceAll("\\s+", "");
+            urlList.add(UrlSeparator.getUrl(tmp).toLowerCase(Locale.ROOT));
         }
+        read.close();
+
         Map<String, Integer> map = createMap(urlList);
 
         List<Helper> helpers = new ArrayList<>();
--- src/com/lesson14/task2/UrlSeparator.java
+++ src/com/lesson14/task2/UrlSeparator.java
@@ -2,17 +2,17 @@ package com.lesson14.task2;
 
 public class UrlSeparator {
 
-    public static String getUrl (String str)  {
+    public static String getUrl(String str) {
         StringBuilder sb = new StringBuilder();
         sb.append(str);
-        if (sb.length() > 7 && sb.substring(0,7).contentEquals("http://")) {
-            sb.delete(0,7);
+        if (sb.length() > 7 && sb.substring(0, 7).contentEquals("http://")) {
+            sb.delete(0, 7);
         }
-        if (sb.length() >4 && sb.substring(0,4).contentEquals("www.")) {
-            sb.delete(0,4);
+        if (sb.length() > 4 && sb.substring(0, 4).contentEquals("www.")) {
+            sb.delete(0, 4);
         }
-        if (str.contains("/")){
-            sb.delete(sb.indexOf("/"),sb.length());
+        if (str.contains("/")) {
+            sb.delete(sb.indexOf("/"), sb.length());
         }
         return sb.toString();
     }
